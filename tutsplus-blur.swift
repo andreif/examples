ff9f80e2edb71c02dca62936561f701fcea82b8b
Andrei Fokau <andrei.fokau@neutron.kth.se>
2014-10-08 17:43:36 +0200

    Update tutsplus-blur.swift

--- tutsplus-blur.swift
+++ tutsplus-blur.swift
@@ -1,90 +1,203 @@
-// http://code.tutsplus.com/tutorials/adding-blur-effects-on-ios--cms-21488
+// code.tutsplus.com/tutorials/adding-blur-effects-on-ios--cms-21488
+// github.com/tutsplus/iOSBlurEffects
+
 import UIKit
 
-class BlurController: UIViewController, UIScrollViewDelegate { // UITableViewDataSource, UITableViewDelegate
-    // http://www.raywenderlich.com/55384/ios-7-best-practices-part-1
-    var backgroundImageView: UIImageView?
-    var blurredImageView: UIImageView?
-    var tableView: UITableView?
-    var screenHeight: CGFloat = 0
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.makeKeyAndVisible()
+        
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.rootViewController = BlurController(nibName: nil, bundle: nil)
+        
+        return true
+    }
+}
+
+
+class BlurController: UIViewController, UIScrollViewDelegate {
+    var blurMask: UIView?
+    var blurredBgImage: UIImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.screenHeight = UIScreen.mainScreen().bounds.size.height
-
-        /*
-        let background = UIImage(named: "page1")
-        self.backgroundImageView = UIImageView(image: background)
-        self.backgroundImageView!.contentMode = UIViewContentMode.ScaleAspectFill
-        self.view.addSubview(self.backgroundImageView!)
-        
-        self.blurredImageView = BlurImageView(frame: self.view.frame)
-        self.blurredImageView!.image = background
-        self.blurredImageView!.contentMode = UIViewContentMode.ScaleAspectFill
-        self.blurredImageView!.alpha = 0
-        // background.applyBlur(radius:30 tintColor:[UIColor colorWithWhite:1 alpha:0.2] saturationDeltaFactor:1.5 maskImage:nil]
-        // self.blurredImageView!.setImageToBlur(background, blurRadius:10, completionBlock:nil)
-        self.view.addSubview(self.blurredImageView!)
-        */
-        
-        //self.view.backgroundColor = UIColor.redColor()
     }
-    
+
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
-    
+
     override func loadView() {
-        self.view.addSubview(self.createContentView());
-        self.view.addSubview(self.createHeaderView());
-        self.view.addSubview(self.createScrollView());
+        // inside loadView there is no self.view; its job, in fact, is to create one
+        self.view = UIView(frame: UIScreen.mainScreen().bounds)
+        self.view.backgroundColor = UIColor.whiteColor()
+    
+        self.view.addSubview(self.createContentView())
+        self.view.addSubview(self.createHeaderView())
+        self.view.addSubview(self.createScrollView())
+        
+        // Blurred with UIImage+ImageEffects
+        // self.blurredBgImage!.image = self.blurWithImageEffects(self.takeSnapshotOfView(self.createContentView()))
+
+        // Blurred with Core Image
+        self.blurredBgImage!.image = self.blurWithCoreImage(self.takeSnapshotOfView(self.createContentView()))
+        
+        // Blurring with GPUImage framework
+        // self.blurredBgImage!.image = self.blurWithGPUImage(self.takeSnapshotOfView(self.createContentView()))
+        
+        self.blurMask = UIView(frame: CGRectMake(0, self.view.frame.size.height - 50, self.view.frame.size.width, 50))
+        self.blurMask!.backgroundColor = UIColor.whiteColor()
+        self.blurredBgImage!.layer.mask = self.blurMask!.layer
+    }
+    
+    /*
+    func blurWithImageEffects(image: UIImage) -> UIImage {
+        return image.applyBlur(radius:30, tintColor:[UIColor colorWithWhite:1 alpha:0.2] saturationDeltaFactor:1.5 maskImage:nil];
+    }
+    */
+    
+    /*
+    func blurWithGPUImage(sourceImage: UIImage) -> UIImage {
+        let blurFilter = GPUImageGaussianBlurFilter()
+        blurFilter.blurRadiusInPixels = 30.0
+        
+        // let blurFilter = GPUImageBoxBlurFilter()
+        // blurFilter.blurRadiusInPixels = 20.0
+        
+        // let blurFilter = GPUImageiOSBlurFilter()
+        // blurFilter.saturation = 1.5
+        // blurFilter.blurRadiusInPixels = 30.0
+        
+        return blurFilter.imageByFilteringImage(sourceImage)
+    }
+    */
+    
+    func blurWithCoreImage(sourceImage: UIImage) -> UIImage {
+        let inputImage = CIImage(CGImage: sourceImage.CGImage)
+        
+        // Apply Affine-Clamp filter to stretch the image so that it does not look shrunken when gaussian blur is applied
+        let transform = CGAffineTransformIdentity
+        let clampFilter = CIFilter(name: "CIAffineClamp")
+        clampFilter.setValue(inputImage, forKey: "inputImage")
+        clampFilter.setValue(NSValue(CGAffineTransform: transform), forKey: "inputTransform")
+        
+        // Apply gaussian blur filter with radius of 30
+        let gaussianBlurFilter = CIFilter(name: "CIGaussianBlur")
+        gaussianBlurFilter.setValue(clampFilter.outputImage, forKey: "inputImage")
+        gaussianBlurFilter.setValue(10, forKey: "inputRadius")
+        
+        let context = CIContext(options: nil)
+        let cgImage = context.createCGImage(gaussianBlurFilter.outputImage, fromRect:inputImage.extent())
+        
+        // Set up output context.
+        UIGraphicsBeginImageContext(self.view.frame.size);
+        let outputContext = UIGraphicsGetCurrentContext()
+        CGContextScaleCTM(outputContext, 1.0, -1.0);
+        CGContextTranslateCTM(outputContext, 0, -self.view.frame.size.height);
+        
+        // Draw base image.
+        CGContextDrawImage(outputContext, self.view.frame, cgImage);
+        
+        // Apply white tint
+        CGContextSaveGState(outputContext);
+        CGContextSetFillColorWithColor(outputContext, UIColor(white:1, alpha:0.2).CGColor)
+        CGContextFillRect(outputContext, self.view.frame);
+        CGContextRestoreGState(outputContext);
+
+        // Output image is ready.
+        let outputImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return outputImage
+    }
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.blurMask!.frame = CGRectMake(
+            self.blurMask!.frame.origin.x,
+            self.view.frame.size.height - 50 - scrollView.contentOffset.y,
+            self.blurMask!.frame.size.width,
+            self.blurMask!.frame.size.height + scrollView.contentOffset.y);
+    }
+    
+    func takeSnapshotOfView(view: UIView) -> UIImage {
+        var reductionFactor: CGFloat = 1
+        UIGraphicsBeginImageContext(CGSizeMake(view.frame.size.width/reductionFactor, view.frame.size.height/reductionFactor))
+        view.drawViewHierarchyInRect(CGRectMake(0, 0, view.frame.size.width/reductionFactor, view.frame.size.height/reductionFactor), afterScreenUpdates:true)
+        var image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
     
     func createHeaderView() -> UIView {
-        let headerView = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, 60))
+        var headerView: UIView = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, 60))
+        
+        /*
+        var blur: UIBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        var effectView: UIVisualEffectView = UIVisualEffectView(effect: blur)
+        effectView.frame = CGRectMake(0, 0, self.view.frame.size.width, 70)
+        headerView.addSubview(effectView)
+        */
+        
         headerView.backgroundColor = UIColor(red:229/255.0, green:39/255.0, blue:34/255.0, alpha:0.6)
         let title = UILabel(frame:CGRectMake(0, 20, self.view.frame.size.width, 40))
         title.text = "Dynamic Blur Demo"
+        title.textColor = UIColor(white:1, alpha:1)
+        title.font = UIFont(name:"HelveticaNeue", size:20)
+        title.textAlignment = NSTextAlignment.Center
         headerView.addSubview(title)
+        
         return headerView
     }
     
-    var contentView: UIView?
-    
     func createContentView() -> UIView {
-        self.contentView = UIView(frame:self.view.frame)
+        let contentView = UIView(frame:self.view.frame)
         // Background image
-        let contentImage = UIImageView(frame:self.contentView!.frame)
-        contentImage.image = UIImage(named:"page1")
-        self.contentView!.addSubview(contentImage)
+        let contentImage = UIImageView(frame: contentView.frame)
+        let img = UIImage(data: NSData(contentsOfURL: NSURL(string:"https://raw.githubusercontent.com/tutsplus/iOSBlurEffects/master/BlurDemo/Images.xcassets/demo-bg.imageset/demo-bg.png")!)!)
+        contentImage.image = img
+        contentView.addSubview(contentImage)
     
         let creditsViewContainer = UIView(frame:CGRectMake(self.view.frame.size.width/2 - 65, 335, 130, 130))
         creditsViewContainer.backgroundColor = UIColor.whiteColor()
         creditsViewContainer.layer.cornerRadius = 65
-        self.contentView!.addSubview(creditsViewContainer)
+        contentView.addSubview(creditsViewContainer)
     
         let photoTitle = UILabel(frame:CGRectMake(0, 54, 130, 18))
         photoTitle.text = "Peach Garden"
+        photoTitle.font = UIFont(name:"HelveticaNeue-Light", size:18)
+        photoTitle.textAlignment = NSTextAlignment.Center
+        photoTitle.textColor = UIColor(white:0.4, alpha:1)
         creditsViewContainer.addSubview(photoTitle)
 
         let photographer = UILabel(frame:CGRectMake(0, 72, 130, 9))
         photographer.text = "by Cas Cornelissen"
+        photographer.font = UIFont(name:"HelveticaNeue-Thin", size:9)
+        photographer.textAlignment = NSTextAlignment.Center
+        photographer.textColor = UIColor(white:0.4, alpha:1)
         creditsViewContainer.addSubview(photographer)
 
-        return self.contentView!
+        return contentView
     }
     
     func createScrollView() -> UIView {
         let containerView = UIView(frame:self.view.frame)
     
-        let blurredBgImage = UIImageView(frame:CGRectMake(0, 0, self.view.frame.size.width, 568))
-        blurredBgImage.contentMode = UIViewContentMode.ScaleToFill
-        containerView.addSubview(blurredBgImage)
+        self.blurredBgImage = UIImageView(frame:CGRectMake(0, 0, self.view.frame.size.width, 568))
+        self.blurredBgImage!.contentMode = UIViewContentMode.ScaleToFill
+        containerView.addSubview(self.blurredBgImage!)
     
         let scrollView = UIScrollView(frame:self.view.frame)
+        containerView.addSubview(scrollView)
         scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height*2 - 110)
         scrollView.pagingEnabled = true
-        containerView.addSubview(scrollView)
+        scrollView.delegate = self;
+        scrollView.bounces = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
         
         let slideContentView = UIView(frame:CGRectMake(0, 518, self.view.frame.size.width, 508))
         slideContentView.backgroundColor = UIColor.clearColor()
@@ -92,32 +205,29 @@ class BlurController: UIViewController, UIScrollViewDelegate { // UITableViewDat
         
         let slideUpLabel = UILabel(frame:CGRectMake(0, 6, self.view.frame.size.width, 50))
         slideUpLabel.text = "Photo information"
+        slideUpLabel.font = UIFont(name:"HelveticaNeue-Light", size:18)
+        slideUpLabel.textAlignment = NSTextAlignment.Center
+        slideUpLabel.textColor = UIColor(white:0, alpha:0.5)
         slideContentView.addSubview(slideUpLabel)
         
         let slideUpImage = UIImageView(frame:CGRectMake(self.view.frame.size.width/2 - 12, 4, 24, 22.5))
-        slideUpImage.image = UIImage(named: "up-arrow.png")
+        let img = UIImage(data: NSData(contentsOfURL: NSURL(string:"https://raw.githubusercontent.com/tutsplus/iOSBlurEffects/master/BlurDemo/Images.xcassets/up-arrow.imageset/up-arrow.png")!)!)
+        slideUpImage.image = img
         slideContentView.addSubview(slideUpImage)
         
         let detailsText = UITextView(frame:CGRectMake(25, 100, 270, 350))
-        detailsText.text = "Lorem ipsum ... laborum"
+        detailsText.backgroundColor = UIColor.clearColor()
+        detailsText.text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        detailsText.font = UIFont(name: "HelveticaNeue-Light", size:16)
+        detailsText.textAlignment = NSTextAlignment.Center
+        detailsText.textColor = UIColor(white:0, alpha:0.6)
         slideContentView.addSubview(detailsText)
         
         return containerView
     }
 }
 
-class BlurImageView: UIImageView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        var blur: UIBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
-        var effectView: UIVisualEffectView = UIVisualEffectView(effect: blur)
-        effectView.frame = frame
-        addSubview(effectView)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-}
-
+// pivotallabs.com/extracting-uiviews-uiviewcontrollers-swift/
+// raywenderlich.com/55384/ios-7-best-practices-part-1
+// stackoverflow.com/questions/17041669/creating-a-blurring-overlay-view
+// stackoverflow.com/questions/25814458/cannot-set-contentsize-of-an-uiscrollview-created-via-loadview
